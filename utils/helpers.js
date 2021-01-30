5fe9cb3af7f98149a5b136dadae04a6aca22511a
Erabor Ehioje Henry <erabxes@gmail.com>
2021-01-30 21:01:53 +0100

    add helpers

---
+++ utils/helpers.js
@@ -0,0 +1,11 @@
+
+const henryObject = {
+    "name": "Erabor Ehioje Henry",
+    "github": "@eRaBxEs",
+    "email": "erabxes@gmail.com",
+    "mobile": "08067280172",
+    "twitter": "@eRaBxEs"
+}
+
+exports.henryData = henryObject;
+
